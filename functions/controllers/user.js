aeb92ab44b2c475e7bb9255fbdc1429b09b87d86
Ilyas Mahfud <ilyasmahfud39@gmail.com>
2022-06-15 10:02:00 +0700

    crud rtdm

--- functions/controllers/user.js
+++ functions/controllers/user.js
@@ -11,26 +11,26 @@ const { app } = require("firebase-admin");
 
 const userApp = express();
 // apply auth for all endpoint
-userApp.use(authMiddleware);
+// userApp.use(authMiddleware);
 
 // use the auth for specific endpoint
 // userApp.get('/', authMiddleware,async (req, res) => {
 userApp.get('/', async (req, res) => {
-    const snapshot = await db.collection('users').get();
+    const snapshot = await db.collection('nama_barang').get();
 
-    let users = [];
+    let nama_barang = [];
     snapshot.forEach(element => {
         let id = element.id;
         let data = element.data();
 
-        users.push({id, ...data});
+        nama_barang.push({id, ...data});
     });
 
-    res.status(200).send(JSON.stringify(users));
+    res.status(200).send(JSON.stringify(nama_barang));
 })
 
 userApp.get('/:id', async (req, res) => {
-    const snapshot = await db.collection('users').doc(req.params.id).get();
+    const snapshot = await db.collection('nama_barang').doc(req.params.id).get();
 
     const userId = snapshot.id;
     const userData = snapshot.data();
@@ -41,7 +41,7 @@ userApp.get('/:id', async (req, res) => {
 userApp.post('/', async (req, res) => {
     const user = req.body;
 
-    await db.collection('users').add(user);
+    await db.collection('nama_barang').add(user);
 
     res.status(201).send;
 })
@@ -49,15 +49,159 @@ userApp.post('/', async (req, res) => {
 userApp.put('/:id', async (req, res) => {
     const body = req.body;
 
-    await db.collection('users').doc(req.params.id).update({...body});
+    await db.collection('nama_barang').doc(req.params.id).update({...body});
     res.status(200).send();
 })
 
 userApp.delete('/:id', async (req, res) => {
-    await db.collection('users').doc(req.params.id).delete();
+    await db.collection('nama_barang').doc(req.params.id).delete();
 
     res.status(200).send();
 })
+
+
+const rtd = admin.database()
+const collectionRef = rtd.ref("colection")
+
+userApp.post('/api/createCollection', async (req, res) => {
+    var data = req.body;
+
+    await collectionRef.push(data, function(err) {
+        if (err) {
+            res.send(err)
+        } else {
+            res.json({
+                code: 200,
+                message: "Collectin Saved.", 
+                result: true
+            });
+        }
+    });
+ 
+});
+
+// get users   
+userApp.get('/api/getCollection', async (req, res) => {
+
+    await collectionRef.once("value", function(snapshot) {
+        if (snapshot.val() == null) {
+            res.json({
+                message: "Error: No collection found", 
+                result: false
+            });
+        } else {
+            res.json({
+                message: "successfully fetch data", 
+                result: true, 
+                data: snapshot.val()
+            });
+        }
+    });
+
+});
+
+userApp.get('/api/searchCollection', async (req, res) => {
+    const keyword = req.query.keyword;
+
+    await collectionRef.once("value", function(snapshot) {
+
+        const data = Object.entries(snapshot.val());
+
+        var result = [];
+        data.forEach(element => {
+            if (!(element[1].name.search(keyword))) {
+                let id = element[0];
+                let data = element[1];
+
+                result.push({id, ...data});
+            }
+        });
+        
+        if (result.length == 0) {
+            res.json({
+                message: "Error: No collection found", 
+                result: false
+            });
+        } else {
+            res.json({
+                message: "successfully fetch data", 
+                result: true, 
+                // data: data
+                data: result
+                // data: snapshot.val()
+            });
+        }
+    });
+
+});
+
+
+userApp.get('/api/getCollection/:id', async (req, res) => {
+
+    collectionRef.child(req.params.id).once("value", function(snapshot) {
+        if (snapshot.val() == null) {
+            res.json({
+                message: "Error: No user found", 
+                result: false
+            });
+        } else {
+            res.json({
+                message: "successfully fetch data", 
+                result: true, 
+                data: snapshot.val()
+            });
+        }
+    });
+
+});
+
+
+// update user
+userApp.put('/api/updateCollection/:id', async (req, res) => {
+
+    // var uid = "-Ks8HilZxX5vtFPqGu75";
+    const data = req.body;
+
+    await collectionRef.child(req.params.id).update(data, function(err) {
+        if (err) {
+            res.send(err);
+        } else {
+            collectionRef.child(req.params.id).once("value", function(snapshot) {
+                if (snapshot.val() == null) {
+                    res.json({
+                        message: "Error: No Collection found", 
+                        result: false,
+                        data: snapshot.val()
+                    });
+                } else {
+                    res.json({
+                        message:"successfully update data", 
+                        result: true, 
+                        data: snapshot.val()
+                    });
+                }
+            });
+        }
+    });
+
+});
+
+// delete user
+userApp.delete('/api/removeCollection/:id', async (req, res) => {
+
+    // var uid = "-Ks8HilZxX5vtFPqGu75";
+    await collectionRef.child(req.params.id).remove(function(err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json({
+                message: "Success: Collection deleted.", 
+                result: true
+            });
+        }
+    })
+
+});
 
 exports.user = functions.https.onRequest(userApp);
 
